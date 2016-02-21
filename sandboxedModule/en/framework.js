81b0d17b1f34e9a6b819e18290385cabce6eacc2
tshemsedinov <timur.shemsedinov@gmail.com>
2016-02-21 10:20:46 +0200

    Sandboxed application example

--- sandboxedModule/en/framework.js
+++ sandboxedModule/en/framework.js
@@ -1,26 +1,26 @@
-// Файл, демонстрирующий то, как фреймворк создает среду (песочницу) для
-// исполнения приложения, загружает приложение, передает ему песочницу в
-// качестве глобального контекста и получает ссылку на экспортируемый
-// приложением интерфейс
+// Example showing us how the framework creates an environment (sandbox) for
+// appication runtime, load an application code and passes a sandbox into app
+// as a global context and receives exported application interface
 
-// Фреймворк может явно загрузить библиотеки
+// The framework can require core libraries
 var fs = require('fs'),
     vm = require('vm');
 
-// создаем контекст-песочницу, которая станет глобальным контекстом приложения
+// Create a hash and turn it into the sandboxed context which will be
+// the global context of an application
 var context = { module: {}, console: console };
 context.global = context;
 var sandbox = vm.createContext(context);
 
-// читаем исходный код приложения из файла
+// Read an application source code from the file
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
-  // Тут нужно обработать ошибки
+  // We need to handle errors here
   
-  // Запускаем код приложения в песочнице
+  // Run an application in sandboxed context
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
   
-  // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
-  // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+  // We can access a link to exported interface from sandbox.module.exports
+  // to execute, save to the cache, print to console, etc.
 });
