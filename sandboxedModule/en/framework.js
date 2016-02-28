8ecbf3219ce6ff4c3e57c27a27da727e1b0b1019
IvanEh <ivanehreshi@gmail.com>
2016-02-28 19:26:13 +0200

    Implement custom framework

--- sandboxedModule/en/framework.js
+++ sandboxedModule/en/framework.js
@@ -6,21 +6,123 @@
 var fs = require('fs'),
     vm = require('vm');
 
+var injections = {};
+var apis = {
+    local: [],
+    global: []
+};
+var configurations = {};
+var mainScriptFile;
+var frameworkLoaded = false;
+
 // Create a hash and turn it into the sandboxed context which will be
 // the global context of an application
-var context = { module: {}, console: console };
-context.global = context;
+var context = { module: {}, console: {log: console.log} };
+    context.global = context;
 var sandbox = vm.createContext(context);
 
+
 // Read an application source code from the file
-var fileName = './application.js';
-fs.readFile(fileName, function(err, src) {
-  // We need to handle errors here
+var fileName = './'
+if(process.argv[2] != undefined) {
+    fileName += process.argv[2]
+} else {
+    fileName += 'application.js'
+}
+
+
+
+function Api( api ) {
+    if(typeof(api) === 'object') {
+        if(api.name.startsWith('./')) {
+            apis.local[api.name] = api;
+            api.resolved = false;
+        } else {
+            apis.global.push(api);
+            api.resolved = false;
+        }
+    }
+}
+
+function Main ( path ) {
+    if(typeof(path) === 'string') {
+            mainScriptFile = path;
+    }
+}
+
+function copy(dest, src) {
+    if(typeof(src) === 'object' && typeof(dest) == 'object') {
+        for(key in src) {
+            dest[key] = src[key];
+        }
+    }
+}
+
+function decorate(f, before, after) {
+  return function() {
+    if(before != undefined) {
+      before.apply(this, arguments);
+    }
+    f.apply(arguments);
+    if(after != undefined) {
+      after.apply(this, arguments);
+    }
+  }
+}
+
+function resolveApi( api ) {
+   var apiObj = apis.local [ api ];
+   if(!apiObj) {
+     apiObj = apis.local [api] = {};
+   }
+   var resolvedContext = {};
   
-  // Run an application in sandboxed context
-  var script = vm.createScript(src, fileName);
-  script.runInNewContext(sandbox);
-  
-  // We can access a link to exported interface from sandbox.module.exports
-  // to execute, save to the cache, print to console, etc.
-});
+   var resolvedImport = apiObj.imports ? apiObj.imports : {};
+   copy(resolvedContext, resolvedImport);
+   delete apiObj.imports; 
+
+   resolvedContext.module = {};
+   resolvedContext.global = resolvedContext;
+   
+   apiObj.resolvedContext = resolvedContext;
+
+   return apiObj;
+}
+
+function configure () {
+      if( !mainScriptFile ) {
+        return;
+      }
+
+    fs.readFile(mainScriptFile, function(err, src) {
+      // We need to handle errors here
+      if(err) {
+        console.log(err);
+      }
+      // Run an application in sandboxed context
+
+      var script = vm.createScript(src, mainScriptFile);
+      var sandbox = vm.createContext(resolveApi(mainScriptFile).resolvedContext);
+      script.runInNewContext(sandbox);
+
+      // We can access a link to exported interface from sandbox.module.exports
+      // to execute, save to the cache, print to console, etc.
+    });
+}
+
+function load() {
+  if(!frameworkLoaded) {
+    copy(global, module.exports);
+    frameworkLoaded  = true;
+  } 
+}
+
+/*
+*/
+
+module.exports = {
+    load: load,
+    configure: configure,
+    Api: Api,
+    Main: Main
+}
