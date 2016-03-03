cf002b5d95cc4669f9848efca79208277b805531
NymphetamineOverdose <kaktusad@gmail.com>
2016-03-03 04:05:40 +0200

    tasks 1-4 w/ bugs

--- sandboxedModule/ru/application2.js
+++ sandboxedModule/ru/application2.js
@@ -3,20 +3,18 @@
 // кусочком фреймворка. Читайте README.md в нем задания.
 
 // Вывод из глобального контекста модуля
-console.log('From application global context');
+console.log('From application2 global context');
 
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
-  console.log('From application exported function');
+  console.log('From application2 exported function');
 };
 
-util.log("util <3");
-
-interval = setInterval(function() {
-	console.log("One Big mac pls...");
+interval2 = setInterval(function() {
+	console.log("One shaverma pls...");
 }, 500);
 
 setTimeout(function() {
-	clearInterval(interval);
+	clearInterval(interval2);
 	console.log("That is all!");
-}, 2500);
+}, 1500);
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -6,21 +6,51 @@
 // Фреймворк может явно зависеть от библиотек через dependency lookup
 var fs = require('fs'),
     vm = require('vm');
+    util = require('util');
+    path = require('path');
 
 // Создаем контекст-песочницу, которая станет глобальным контекстом приложения
-var context = { module: {}, console: console };
-context.global = context;
-var sandbox = vm.createContext(context);
+var context = {
+    module: {},
+    console: console,
+    setInterval: setInterval,
+    setTimeout: setTimeout,
+    clearInterval: clearInterval,
+    util: util
+  };
 
-// Читаем исходный код приложения из файла
-var fileName = './application.js';
-fs.readFile(fileName, function(err, src) {
-  // Тут нужно обработать ошибки
-  
-  // Запускаем код приложения в песочнице
-  var script = vm.createScript(src, fileName);
-  script.runInNewContext(sandbox);
-  
-  // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
-  // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+context.global = context;
+
+// Wrapper for console.log
+function wrapped(filename) {
+  if(console.log) {
+    var oldConsoleLog = console.log;
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    console.log = function(){
+      Array.prototype.unshift.call(arguments, '[', day, month, year, filename, ']:');
+      oldConsoleLog.apply(this, arguments);
+    }
+  }
+};
+process.argv.slice(2).forEach((filename) => {
+  var sandbox = vm.createContext(context);
+
+  wrapped(filename);
+
+  // Читаем исходный код приложения из файла
+  fs.readFile(filename, function(err, src) {
+    // Тут нужно обработать ошибки
+
+    // Запускаем код приложения в песочнице
+    var script = vm.createScript(src, filename);
+    script.runInNewContext(sandbox);
+
+    // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
+    // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+  });
 });
