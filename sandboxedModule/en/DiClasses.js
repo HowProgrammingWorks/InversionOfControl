7f2102949a24099d1eb0f33ed5dbbff60c9d099d
IvanEh <ivanehreshi@gmail.com>
2016-03-05 14:15:07 +0200

    Add App class to DiClasses

---
+++ sandboxedModule/en/DiClasses.js
@@ -0,0 +1,9 @@
+function Api(name, imports, injectedApis) {
+    if(typeof(name) === '
+    this.resolved = false; // is dependencies computed?
+    this.name = name || null; // file name and api name at the same time
+    this.imports = imports || {}; // fields and functions that need to be injected 
+    this.resolvedImport = {}; // partially computed context
+    this.resolvedContext = {}; // context that will be injected to the file
+}
+
