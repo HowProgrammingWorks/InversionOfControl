3d886506a3e56881ad898ae72e9a284cd36f282c
IvanEh <ivanehreshi@gmail.com>
2016-03-05 15:23:22 +0200

    Change Api constructor

--- sandboxedModule/en/DiClasses.js
+++ sandboxedModule/en/DiClasses.js
@@ -1,13 +1,27 @@
-function Api(name, imports, injectedApis) {
-    if(typeof(name) === '
+function Api(name, imports, injectedApis, injections, lookup) {
     this.resolved = false; // is dependencies computed?
     this.name = name || null; // file name and api name at the same time
     this.imports = imports || {}; // fields and functions that need to be injected 
     this.resolvedImport = {}; // partially computed context
     this.resolvedContext = {}; // context that will be injected to the file
+    this.injections = injections || {};
+    this.lookup = lookup || lookupType(name);
+
+    if(typeof(injectedApis) === 'string') {
+        injectedApis = [injectedApis];
+    }
+    this.injectedApis = injectedApis || [];
 }
 
-module.exports {
+function lookupType(name) {
+    if(name.startsWith('./')) {
+        return 'GLOBAL'
+    } else {
+        return 'LOCAL'
+    }
+}
+
+module.exports = {
     Api: Api,
 }
 
