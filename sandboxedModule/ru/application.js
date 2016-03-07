9b374ec9d76875111a2b0eb2474c1f89c27b3664
Romm17 <romanysik@gmail.com>
2016-03-07 10:50:58 +0200

    task2 done

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -5,7 +5,17 @@
 // Вывод из глобального контекста модуля
 console.log('From application global context');
 
+var Person = function(name, age) {
+	this.name = name;
+	this.age = age;
+	return this;
+};
+
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
-  console.log('From application exported function');
+  var me = new Person("Roman", 18);
+  console.log(util.format(
+  	"Calling util.inspect using util.format: %s",
+  	util.inspect(me))
+  );
 };
