102f5763406cd39bae3a218788ce745fe999eda5
Romm17 <romanysik@gmail.com>
2016-03-09 19:07:46 +0200

    Task 5 done

--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -11,7 +11,12 @@ var fs = require('fs'),
 var fileName = './application.js';
 var applicationConsole = {};
 applicationConsole.log = function(str) { 
-	console.log(fileName + " " + (new Date()) + " " + str); 
+	fs.appendFile('log.txt',
+	 fileName + " " + (new Date()) + " " + str + '/n',
+	 function(err) {
+	 	if(err)
+	 		throw err;
+	 });
 	console.log(str);
 };
 // applicationConsole.log = function(str) {
