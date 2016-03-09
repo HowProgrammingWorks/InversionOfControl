4cfb023396a1b1ab130c4a17a530eae8965b844b
IvanEh <ivanehreshi@gmail.com>
2016-03-09 18:28:29 +0200

    Make task4 to use the utility module

--- sandboxedModule/en/task4/taskConfig.js
+++ sandboxedModule/en/task4/taskConfig.js
@@ -1,15 +1,18 @@
 require('../framework').load();
 path = require("path")
+var diUtils = require("../DiUtils.js");
 
 Api ({
   name: './task.js',
 
   imports: {
-    console: {
-      log: (mess) => { console.log( "<" + path.basename(process.argv[1]) + "> "   + mess) }
-    }
+     console: diUtils.merge(console, {
+              log: (mess) => {
+                  console.log( "<" + path.basename(process.argv[1]) + "> "   + mess) 
+                  }
+             })
   }
-}) 
+});
 
 Main ('./task.js')
 
