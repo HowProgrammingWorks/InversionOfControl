8bdf1a506caac75190b27f781d4089ef21591ca5
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-03-10 14:40:58 +0200

    Solve task1

--- sandboxedModule/en/application.js
+++ sandboxedModule/en/application.js
@@ -5,6 +5,14 @@
 // Print from the global context of application module
 console.log('From application global context');
 
+setInterval(() => {
+  console.log('[setInterval] Hello again!');
+}, 1000);
+
+setTimeout(() => {
+  console.log('[setTimeout]  Hello once!');
+}, 500);
+
 module.exports = function() {
 	// Print from the exported function context
   console.log('From application exported function');
