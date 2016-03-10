19b68954a5cba7ba1c12ec885ee3723295134928
Roman Statkevych <romanstatkevich@gmail.com>
2016-03-10 12:04:01 +0200

    first part of 2

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,17 +1,11 @@
-// Вывод из глобального контекста модуля
-console.log('From application global context');
-
-// Объявляем функцию для события таймера
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);
-
 // Пример использования fs API
+
+// ensure that fs is in context
+//console.log(fs)
+
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
+
 fs.readFile(fileName, function(err, src) {
-  console.log('File ' + fileName + ' size ' + src.length);
+  1console.log('File ' + fileName + ' size ' + src.length);
 });
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -3,28 +3,55 @@
 var fs = require('fs'),
     vm = require('vm');
 
+var statistics = {
+  count:0,
+  times: []
+};
+
+function grabStatistics() {
+  var avg = statistics.times.reduce(function(a,b){return a+b;}, 0)/statistics.count;
+  console.log('Average: '+avg);
+  console.log('Total calls: '+statistics.count);
+}
+
+function wrapFunction(fnName, fn) {
+  return function wrapper() {
+    var args = [];
+    Array.prototype.push.apply(args, arguments);
+    var callback = args[args.length-1];
+    
+    if(typeof(callback) === 'function') {
+      args[args.length-1] = wrapFunction(callback.name, args[args.length-1]);
+    }  
+    console.log('Call: ' + fnName);
+    //console.dir(args);
+
+    var start = (new Date()).now();
+    var data = fn.apply(undefined, args);
+    var end = (new Date()).now() - start;
+    statistics.times.push(end);
+    statistics.count++;
+    return data;
+  }
+}
+
+function cloneInterface(anInterface) {
+  var clone = {};
+  for (var key in anInterface) {
+    if(typeof(anInterface[key])==='function')
+      clone[key] = wrapFunction(key,anInterface[key]);
+  }
+  return clone;
+}
+
 // Объявляем хеш из которого сделаем контекст-песочницу
 var context = {
   module: {},
   console: console,
   // Помещаем ссылку на fs API в песочницу
-  fs: fs,
-  // Оборачиваем функцию setTimeout в песочнице
-  setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
-      console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
+  fs: cloneInterface(fs),
+  setTimeout: setTimeout,
+  setInterval: setInterval
 };
 
 // Преобразовываем хеш в контекст
@@ -37,4 +64,5 @@ fs.readFile(fileName, function(err, src) {
   // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
+  setInterval(grabStatistics, 30000)
 });
