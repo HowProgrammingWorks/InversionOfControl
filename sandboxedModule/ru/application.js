89d1927d11bdb9d1138c9e9ef8a3f14d7702bc80
= <=>
2016-03-11 16:21:34 +0200

    Task1

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -4,6 +4,8 @@
 
 // Вывод из глобального контекста модуля
 console.log('From application global context');
+setTimeout(function(){ console.log('!!!'); },1000);
+setInterval(function(){ console.log('!!!'); },1000);
 
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
