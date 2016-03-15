6ac58fa781744d2ce58a5d57e884018b17943884
LCorncrake <luyda.derkach@gmail.com>
2016-03-15 16:05:54 +0200

    Task 1-6.5 semi-completed :)

--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -3,28 +3,45 @@
 var fs = require('fs'),
     vm = require('vm');
 
+var fstatistics = {
+	num:0;
+	fileTime:[];
+}
+
+function statisticsOfFile(){
+	
+
+}
+
+function wrapFunction(fnName, fn) {
+    return function wrapper() {
+      var start = new Data();
+      var args = [];
+      Array.prototype.push.apply(args, arguments);
+      var callback = args[args.length-1];
+      if (typeof(callback) === "function")
+		args[args.length-1] = wrapFunction(callback.name, callback);
+      console.log('Call: ' + fnName);
+      console.dir(args);
+      var data = fs.apply(undefined, args);
+      var fin = new Data();
+      return fn.apply(undefined, args);
+    }
+}
+
+function cloneInterface(anInterface){ var clone = {};
+    for (var key in anInterface) {
+      clone[key] = wrapFunction(key, anInterface[key]);
+    }
+    return clone;
+}
 // Объявляем хеш из которого сделаем контекст-песочницу
 var context = {
   module: {},
   console: console,
   // Помещаем ссылку на fs API в песочницу
-  fs: fs,
+  fs: cloneInterface(fs),
   // Оборачиваем функцию setTimeout в песочнице
-  setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
-      console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
 };
 
 // Преобразовываем хеш в контекст
