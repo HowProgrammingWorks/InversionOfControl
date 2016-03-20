87a9bec5721ef462c178b9f09bd4cccc6ac41c44
YuriiKrat <yuriikrat97@gmail.com>
2016-03-20 16:25:21 +0200

    Tasks 1-2 done

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -13,9 +13,19 @@ var Country = function (name, capital) {
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
   //console.log('From application exported function');
+   setTimeout(() => {
+  	console.log("setTimeout function");
+  }, 3000);
+
+  setInterval(() => {
+  	console.log("setInterval function");
+  }, 1000);
+  //Задание 1
+
   var newCountry = new Country("Ukraine", "Kyiv");
   console.log(util.format("Call util.inspect() through util.format(): %s", 
   	util.inspect(newCountry))
   );
+  //Задание 2
 
 };
