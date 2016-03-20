ea00f15c09b98bd0f4b1e395a89aace8e38877b5
RBV <bohdanromanchyc@mail.ru>
2016-03-20 11:35:17 +0200

    lab is done

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -3,9 +3,25 @@
 // кусочком фреймворка. Читайте README.md в нем задания.
 
 // Вывод из глобального контекста модуля
-console.log('From application global context');
+console.log(util.inspect('From application global context'));
 
-module.exports = function() {
-  // Вывод из контекста экспортируемой функции
-  console.log('From application exported function');
+var http = require('http');
+
+var task = function(){
+	setTimeout(function(){
+		console.log('Timeout log task1');
+	}, 1000);
+}
+task();
+
+module.exports = {
+    number: 100,
+    str: 'asadsdasd',
+    obj: {},
+    array: []
 };
+
+module.exports.f = function() {
+   // Вывод из контекста экспортируемой функции
+   console.log('From application exported function');
+ };
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -5,15 +5,61 @@
 
 // Фреймворк может явно зависеть от библиотек через dependency lookup
 var fs = require('fs'),
-    vm = require('vm');
+    vm = require('vm'),
+    util = require('util');
+    //app = require('./application.js');
 
-// Чоздаем контекст-песочницу, которая станет глобальным контекстом приложения
-var context = { module: {}, console: console };
+//Обгортаємо console.log 
+var wrapperLog = function(message){
+    // Завдання 4
+                message = message || '';
+                var date = new Date();
+                var text = fileName;
+                    text+= ' ' + date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+                    text+= ' ' + message;
+                console.log(text);
+    // Завдання 5
+                fs.appendFile("wrapperLog.txt", text+"\r\n", function(err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                })
+            }
+
+var wrapperRequire = function(module){
+    var date = new Date();
+    var text =date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+
+    text+=module;
+    fs.appendFile("wrapperRequire.txt", text+"\r\n", function(err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                })
+    require(module);
+}
+// Cоздаем контекст-песочницу, которая станет глобальным контекстом приложения
+var context = { module: {},
+			    console: {
+                    log: wrapperLog
+                }, 
+                // Завдання 1
+			    setTimeout: setTimeout,
+			    setInterval: setInterval,
+			    //Завдання 2
+                util: util,
+                require: wrapperRequire
+                };
+
+               
+
 context.global = context;
+
 var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
-var fileName = './application.js';
+// Завдання 3
+var fileName = process.argv[2]; // './application.js'
 fs.readFile(fileName, function(err, src) {
   // Тут нужно обработать ошибки
   
@@ -23,4 +69,7 @@ fs.readFile(fileName, function(err, src) {
   
   // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
   // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+    for (var p in sandbox.module.exports) {
+  	console.log(p + " : " + typeof sandbox.module.exports[p]);
+  }
 });
