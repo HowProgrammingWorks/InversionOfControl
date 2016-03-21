6ca94836ee5830e0e9b5d3fca2bb233c7b00165d
Veniamin Zinevych <mitchsvik@gmail.com>
2016-03-21 18:58:24 +0200

    version 0.14.1 #5
    +added some fs functions for testing;

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -7,3 +7,14 @@ fs.readFile(fileName, function(err, src) {
 fs.realpath(fileName, function(err, src) {
     console.log('File: ' + src);
 })
+
+fs.readdir('./', function(err, files) {
+    console.log('Files in local directory: ' + files);
+})
+
+fs.open('./file.txt', 'r', mode=0666, function(err, fd){
+    console.log('File open');
+    fs.close(fd, function(err, param){
+        console.log('File closed')
+    })
+})
