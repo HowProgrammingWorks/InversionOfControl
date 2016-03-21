bd4945dbdfd7415adbef5ed6a27f0e43c4e0fce9
IvanEh <ivanehreshi@gmail.com>
2016-03-21 20:05:09 +0200

    Wrap fs

--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -2,13 +2,14 @@
 
 var fs = require('fs'),
     vm = require('vm');
+var util = require('util');
 
 // Объявляем хеш из которого сделаем контекст-песочницу
 var context = {
   module: {},
   console: console,
   // Помещаем ссылку на fs API в песочницу
-  fs: fs,
+  fs: wrapAll(fs),
   // Оборачиваем функцию setTimeout в песочнице
 };
 
@@ -23,3 +24,48 @@ fs.readFile(fileName, function(err, src) {
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
 });
+
+/**
+ * Utility functions 
+ */
+
+function cloneInterface(anInterface) {
+ var clone = {};
+   for (var key in anInterface) {
+      clone[key] = anInterface[key];
+   }
+      return clone;
+}
+
+function wrapFunction(fnName, fn) {
+ return function wrapper() {
+     var args = [];
+     Array.prototype.push.apply(args, arguments);
+     
+     console.log('Call: ' + fnName);
+     console.log('Args: ' +  args.length);
+     console.log(args);
+     
+     if(typeof args[args.length - 1] === 'function' ) {
+         args[args.length - 1] = wrapFunction('callback',
+                 args[args.length - 1]);
+     }
+
+     fn.apply(undefined, args);
+ }
+}
+
+function wrapAll(api) {
+ var preparedApi = cloneInterface(api);
+ for(var key in preparedApi) {
+    if(typeof preparedApi[key] === 'function') {
+        preparedApi[key] = wrapFunction(key, preparedApi[key]);
+    }
+ }
+ return preparedApi;
+}
+
+/**
+ * Framework logic 
+ */
+
