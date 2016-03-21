175d9db7f3cb2b02f07cfe6ba53c1602f014ceeb
IvanEh <ivanehreshi@gmail.com>
2016-03-21 15:11:14 +0200

    Clean up application.js

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,10 +1,6 @@
-// Вывод из глобального контекста модуля
-console.log('From application global context');
+var fileName = './README.md';
 
-// Объявляем функцию для события таймера
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);
+console.log('Application going to read ' + fileName);
+fs.readFile(fileName, function(err, src) {
+    console.log('File ' + fileName + ' size ' + src.length);
+});
