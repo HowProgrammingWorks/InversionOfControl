07ec4852f9bdf65a8b7a747280cc4aef6e6fa528
Romm17 <romanysik@gmail.com>
2016-03-23 13:08:53 +0200

    task2

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,14 +1,6 @@
 // Вывод из глобального контекста модуля
 console.log('From application global context');
 
-// Объявляем функцию для события таймера
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);
-
 // Пример использования fs API
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
