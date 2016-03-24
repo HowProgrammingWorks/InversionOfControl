01ffd5f593c1e30b45e6ef9b17639e0e7c654945
Oleksandr Finchuk <dragonbest1997@gmail.com>
2016-03-24 12:58:56 +0200

    Update application.js

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -3,9 +3,34 @@
 // кусочком фреймворка. Читайте README.md в нем задания.
 
 // Вывод из глобального контекста модуля
+
 console.log('From application global context');
 
-module.exports = function() {
-  // Вывод из контекста экспортируемой функции
-  console.log('From application exported function');
+//Добавление и удаление ключей
+global.a=50;
+var myStr='str';
+delete myStr1;
+//Task1
+var interval=setInterval(function(){
+	console.log('Repeating text');
+},1000);
+setTimeout(function(){
+	clearInterval(interval);
+
+	//Task2
+	console.log(util.format("Some str: %s ...%d",'STRING',9));
+},4000)
+
+require('path');
+
+
+module.exports = {
+	var1:1,
+	var2:'str',
+	func1: function(b,c){
+		console.log(b);
+	}
+	
+
 };
+
