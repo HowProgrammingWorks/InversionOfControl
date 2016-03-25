f9b30b6cb1da12ea9b9c80a91f126e6644771a5b
= <=>
2016-03-25 16:13:09 +0200

    Task 8

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -16,26 +16,5 @@ module.exports.print = function(text) {
     console.log(text);
 }
 module.exports.variable = 42;
-// Немного таймеров
-var timeout_log = function() {
-    console.log('Timeout: 1 second');
-};
 
-var interval_log = function() {
-    console.log('Interval: 2 second');
-};
-
-var timeout_id = setTimeout(timeout_log, 1000);
-var interval_id = setInterval(interval_log, 2000);
-// Убираем таймер
-var clear_interval = function() {
-    clearInterval(interval_id);
-    console.log('Timer cleared');
-};
-
-setTimeout(clear_interval, 10100);
-// Робота с Util
-util.log('Printed by util.log()');
-console.log(util.format('%s, %s', 'Hello', 'console!'));
-// Вызов супер функции из стороннего файла
 application2.superFunction();
