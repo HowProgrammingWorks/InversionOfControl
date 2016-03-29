020929f285dd5349981e2d1a5f4fcfd2cdacb65d
alex <abalitsky@maprtech.com>
2016-03-29 18:54:06 +0300

    Task 3

--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -21,7 +21,7 @@ context.global = context;
 var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
-var fileName = './application.js';
+var fileName = process.argv[2] || './application.js';
 fs.readFile(fileName, function(err, src) {
   // Тут нужно обработать ошибки
   
