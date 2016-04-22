85dfc1402be13856c7dee44a11a35daa600d6d83
= <=>
2016-04-22 13:44:24 +0300

    Task 2.4

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,5 +1,9 @@
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
 fs.readFile(fileName, function(err, src) {
-  console.log('File ' + fileName + ' size ' + src.length);
+    console.log('File ' + fileName + ' size ' + src.length);
 });
+
+fs.realpath(fileName, function(err, src) {
+    console.log('File: ' + src);
+})
