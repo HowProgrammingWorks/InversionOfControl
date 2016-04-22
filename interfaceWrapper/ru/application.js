a6530b2d49a3be22af79b058e471ac2d632e6154
= <=>
2016-04-22 14:12:45 +0300

    Task 2.6

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,20 +1,45 @@
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
-fs.readFile(fileName, function(err, src) {
-    console.log('File ' + fileName + ' size ' + src.length);
-});
 
-fs.realpath(fileName, function(err, src) {
-    console.log('File: ' + src);
-})
+var readID = setInterval(read, 5000);
+var pathID = setInterval(path, 3000);
+var dirID = setInterval(dir, 10000);
+var openID = setInterval(open, 7000);
 
-fs.readdir('./', function(err, files) {
-    console.log('Files in local directory: ' + files);
-})
+ID = [readID, pathID, dirID, openID];
 
-fs.open('./file.txt', 'r', mode=0666, function(err, fd){
-    console.log('File open');
-    fs.close(fd, function(err, param){
-        console.log('File closed')
-    })
-})
+setTimeout(stop, 60000);
+
+function read(){
+    fs.readFile(fileName, function(err, src) {
+        console.log('File ' + fileName + ' size ' + src.length);
+    });
+}
+
+function path(){
+    fs.realpath(fileName, function(err, src) {
+        console.log('File: ' + src);
+    });
+}
+
+function dir(){
+    fs.readdir('./', function(err, files) {
+        console.log('Files in local directory: ' + files);
+    });
+}
+
+function open(){
+    fs.open('./file.txt', 'r', mode=0666, function(err, fd){
+        console.log('File open');
+        fs.close(fd, function(err, param){
+            console.log('File closed')
+        })
+    });
+}
+
+function stop(){
+    for (var i = 0; i<ID.length; i++){
+        clearInterval(ID[i]);
+    }
+    console.log('Timers stoped');
+}
