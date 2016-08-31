6c003f276296195cbb444e44c426f0e00a43c6a2
grymyk <grymyk@gmail.com>
2016-08-31 16:14:37 +0300

    ass util module

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -19,6 +19,8 @@ module.exports = {
 
 	clearInterval: (timer) => {
 		console.log('Clear Interval Timer: ', timer);
-	}
+	},
+
+	util: util
 };
 
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -1,44 +1,45 @@
 'use strict';
 
-// Файл, демонстрирующий то, как фреймворк создает среду (песочницу) для
-// исполнения приложения, загружает приложение, передает ему песочницу в
-// качестве глобального контекста и получает ссылу на экспортируемый
-// приложением интерфейс. Читайте README.md в нем задания.
-
-// Фреймворк может явно зависеть от библиотек через dependency lookup
 let fs = require('fs'),
+	util = require('util'),
     vm = require('vm');
 
-// Создаем контекст-песочницу, которая станет глобальным контекстом приложения
 let context = {
 	module: {},
 	console: console,
 	setTimeout: setTimeout,
 	clearTimeout: clearTimeout,
 	setInterval: setInterval,
-	clearInterval: clearInterval
+	clearInterval: clearInterval,
+	util: util
 };
 
 context.global = context;
 
 let sandbox = vm.createContext(context);
 
-// Читаем исходный код приложения из файла
 let fileName = './application.js';
 
 fs.readFile(fileName, (err, src) => {
-	// Тут нужно обработать ошибки
-  
-  	// Запускаем код приложения в песочнице
-	let script = vm.createScript(src, fileName);
+	if (err) {
+		throw err;
+	}
+ 
+  	let script = vm.createScript(src, fileName);
   	script.runInNewContext(sandbox);
   
-  	// Забираем ссылку из sandbox.module.exports, можем ее исполнить,
-  	// сохранить в кеш, вывести на экран исходный код приложения и т.д.
-	let app = sandbox.module.exports;
+  	let app = sandbox.module.exports;
 
-	timers(app);
+	useUtil(app);
 });
+
+function useUtil(app) {
+	const util = app.util;
+
+	console.log( util.format('Hello %s', 'util') );
+
+	console.log( util.inspect({a: '42', rt: 89}) );
+}
 
 function timers(app) {
 	app.setTimeout((...args) => {
