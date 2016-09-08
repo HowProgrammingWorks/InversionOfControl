0b87e6c1e50732de3585794cf8023f5d98dbbbe0
Mykola Grytsai <grymyk@gmail.com>
2016-09-08 10:13:09 +0300

    set to ES6

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,10 +1,13 @@
+'use strict';
+
 // Вывод из глобального контекста модуля
 console.log('From application global context');
 
 // Объявляем функцию для события таймера
 function timerEvent() {
-  console.log('From application timer event');
+	console.log('From application timer event');
 }
 
 // Устанавливаем функцию на таймер
 setTimeout(timerEvent, 1000);
+
